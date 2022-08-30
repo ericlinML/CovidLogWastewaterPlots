6970668c0d9a12a3d9a0799662aa2c55374ecb12
Eric L <email.e.lin@gmail.com>
2022-08-30 00:00:07 -0400

    added facet plots

--- run.py
+++ run.py
@@ -53,6 +53,26 @@ def plot_linear_covid(county):
     
     return(temp_plot)
 
+def plot_log_facet(county_list):
+    temp_df = df[df['name'].isin(county_list)]
+    
+    temp_plot = (ggplot(temp_df, aes('sampling_week', 'effective_concentration_rolling_average', group=1))
+     + geom_point()
+     + geom_line()
+     + p9.facet_wrap('name', ncol=1)
+     + p9.scale_x_datetime(breaks=date_breaks('1 month'), labels=date_format('%D'), name='Date')
+     + p9.scale_y_log10(name = 'Rolling average COVID concentration in wastewater')
+     + p9.ggtitle(f'Log Scale COVID Wastewater Data up to {temp_df.sampling_week.to_list()[-1]}')
+     + p9.theme(
+         figure_size=(24,24),
+         axis_title=element_text(size=14),
+         plot_title=element_text(size=18),
+         axis_text_x=element_text(size=7)
+         )
+    )
+
+    return(temp_plot)
+
 
 df = pd.read_csv('~/projects/covid/wastewater_by_county.csv')
 last_upload_date = pd.read_csv('~/projects/covidLogPlot/last_upload_date.csv')
@@ -62,8 +82,8 @@ last_date = last_upload_date.iloc[-1, 0]
 last_update = df[df['name'] == 'Suffolk County, MA']['sampling_week'].to_list()[-1]
 print(last_update)
 
-if last_date != last_update:
-#if last_date == last_update: # for testing 
+#if last_date != last_update:
+if last_date == last_update: # for testing 
 
     # clear old images
     try: 
@@ -83,22 +103,44 @@ if last_date != last_update:
         'Bristol County, MA',
         'Hampden County, MA'
         ]
+
+    # splitting up counties into older and newer counties for facet plots    
+    old_counties = [
+        'Suffolk County, MA',
+        'Essex County, MA',
+        'Nantucket County, MA',
+        'Hampshire County, MA',
+        'Middlesex County, MA',
+        ]
+
+    new_counties = [
+        'Worcester County, MA',
+        'Plymouth County, MA',
+        'Barnstable County, MA',
+        'Bristol County, MA',
+        ]
             
     df[df['state']=='MA'].name.value_counts().index.to_list()
     county_data = {}
 
+    # plot individual plots
     for county in ma_counties:
         county_name = county.split()[0].lower()
         print(county_name)
         log_save_string = f"figures/{county_name}_log_{last_update}.jpg"
         linear_save_string = f"figures/{county_name}_linear_{last_update}.jpg"
         
-        logPlot = plot_log_covid(county)
-        logPlot.save(log_save_string, dpi=300)
-        linearPlot = plot_linear_covid(county)
-        linearPlot.save(linear_save_string, dpi=300)
+        log_plot = plot_log_covid(county)
+        log_plot.save(log_save_string, dpi=300)
+        linear_plot = plot_linear_covid(county)
+        linear_plot.save(linear_save_string, dpi=300)
 
         county_data[county] = {'log': log_save_string, 'linear':linear_save_string}
+
+    # plot facet plots
+    for group_name, group in zip(['old_counties', 'new_counties'], [old_counties, new_counties]):
+        group_plot = plot_log_facet(group)
+        group_plot.save(f"figures/{group_name}_log_{last_update}.jpg", dpi=300) 
     
     last_upload_date.loc[len(last_upload_date.index), 'date'] = last_update
     last_upload_date.to_csv('last_upload_date.csv', index=False)
@@ -118,17 +160,21 @@ if last_date != last_update:
         refresh_token=creds['refresh_token']
         )
 
-    subr = 'CoronavirusMa'
-#    subr = 'test'
+#    subr = 'CoronavirusMa'
+    subr = 'test'
     subreddit = reddit.subreddit(subr)
     title = f'{last_update} COVID-19 Wastewater Log & Linear Plots for MA Counties using Biobot data'
-    images = []
+    images = [
+        {'image_path': f"figures/old_counties_log_{last_update}.jpg", 'caption': f'Multi-County Log Plot'},
+        {'image_path': f"figures/new_counties_log_{last_update}.jpg", 'caption': f'Multi-County Log Plot'},
+         ]
+
     for county in ma_counties:
         images.append({'image_path': county_data[county]['log'], 'caption': f'{county.split()[0]} County Log Plot'})
         images.append({'image_path': county_data[county]['linear'], 'caption': f'{county.split()[0]} County Linear Plot'})
 
-    subreddit.submit_gallery(title, images, flair_id='1d8891e0-80e4-11ea-8ad7-0e20863e7c8d')
-#    subreddit.submit_gallery(title, images,)
+#    subreddit.submit_gallery(title, images, flair_id='1d8891e0-80e4-11ea-8ad7-0e20863e7c8d')
+    subreddit.submit_gallery(title, images,)
 
     # Setting up to send email
     port = 465
@@ -150,8 +196,8 @@ if last_date != last_update:
     image_files = [
         f'figures/suffolk_log_{last_update}.jpg', 
         f'figures/suffolk_linear_{last_update}.jpg', 
-        f'figures/middlesex_log_{last_update}.jpg', 
-        f'figures/middlesex_linear_{last_update}.jpg',
+        f'figures/old_counties_log_{last_update}.jpg',
+        f'figures/new_counties_log_{last_update}.jpg',
 ]
     media_ids = []
     for image_file in image_files:
@@ -159,7 +205,7 @@ if last_date != last_update:
         media_ids.append(id.media_id)
 
     tweetText = f"""
-    Updated COVID-19 Wastewater Log & Linear Plots for Suffolk and Middlesex Counties, MA using Biobot data
+    Updated COVID-19 Wastewater Log & Linear Plots for Suffolk County and broadly MA using Biobot data
 
     Data goes up to {last_update}
 
